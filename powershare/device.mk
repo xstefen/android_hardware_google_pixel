4ef607c4b9ff44b186e2d661faa1cbf2b2510845
Stefen Wakefield <me@xstefen.dev>
2023-06-08 11:55:30 -0500

    Remove powershare and touch modules

--- powershare/device.mk
+++
@@ -1,5 +0,0 @@
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.pixel
-
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powershare
--- touch/device.mk
+++
@@ -1,5 +0,0 @@
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.pixel
-
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/touch
